5e9cbf575c1953996be102ebbb4e400ab6d83e51
fastjrun <fastjrun@139.com>
2018-11-18 10:09:02 +0800

    调整测试数据和参数

--- fast-demo_data.sql
+++ fast-demo_data.sql
@@ -8,9 +8,6 @@ insert into t_user(`id`,`loginPwd`,`nickName`,`sex`,`mobileNo`,`loginErrCount`,`
  ' ' ||
  '08:29:43.382','fastjrun2','2018-11-18 08:29:43.382','12810002002@128.com',null,1);
 
- insert into t_user_login(`id`,`userId`,`deviceId`,`loginCredential`,`createTime`,`logOutTime`,`lastLoginTime`,
- `inValidateTime`,`status`) values(1,1,'unittest1','uuid1','2018-11-18 08:29:43.382',null,'2018-11-18 08:29:43.382',
- null,1);
- insert into t_user_login(`id`,`userId`,`deviceId`,`loginCredential`,`createTime`,`logOutTime`,`lastLoginTime`,
- `inValidateTime`,`status`) values(1,2,'unittest2','uuid2','2018-11-18 08:29:43.382',null,'2018-11-18 08:29:43.382',
- null,1);
+ insert into t_user_login(`id`,`userId`,`deviceId`,`loginCredential`,`createTime`,`logOutTime`, `inValidateTime`,`status`) values(1,1,'unittest1','uuid1','20181118082943382',null,null,1);
+
+ insert into t_user_login(`id`,`userId`,`deviceId`,`loginCredential`,`createTime`,`logOutTime`,`inValidateTime`,`status`) values(2,2,'unittest2','uuid2','20181118082943382',null,null,1);
