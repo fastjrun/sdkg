0fdb7041f73107e70cc232a4ac51ae3c46bd6ce1
fastjrun <fastjrun@139.com>
2019-04-27 19:09:46 +0800

    重构测试基类

--- sdkg-test/src/main/java/com/fastjrun/test/AbstractAdVancedTestNGSpringContextTest.java
+++ sdkg-test/src/main/java/com/fastjrun/test/AbstractAdVancedTestNGSpringContextTest.java
@@ -3,14 +3,9 @@
  */
 package com.fastjrun.test;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.Set;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fastjrun.test.util.TestUtils;
+import com.fastjrun.utils.JacksonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
@@ -20,29 +15,28 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fastjrun.test.util.TestUtils;
-import com.fastjrun.utils.JacksonUtils;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.util.Properties;
 
-@ContextConfiguration(locations = {"classpath*:applicationContext.xml"})
-public abstract class AbstractAdVancedTestNGSpringContextTest extends
-        AbstractTestNGSpringContextTests {
+@ContextConfiguration(locations = { "classpath*:applicationContext.xml" })
+public abstract class AbstractAdVancedTestNGSpringContextTest
+  extends AbstractTestNGSpringContextTests {
 
-    protected static final String ASSERTION_SPLITSTRING = ",assert=";
-    protected static final int PARAM_ASSERTION_ARRAY_SIZE = 2;
+    protected static final String ASSERTION_SPLITSTRING      = ",assert=";
+    protected static final int    PARAM_ASSERTION_ARRAY_SIZE = 2;
 
     protected final Logger log = LoggerFactory.getLogger(this.getClass());
 
     protected Properties propParams = new Properties();
 
     @BeforeClass
-    @Parameters({
-            "envName"
-    })
+    @Parameters({ "envName" })
     protected void initParam(@Optional("unitTest") String envName) {
         try {
             InputStream inParam =
-                    this.getClass().getResourceAsStream((("/testdata/" + envName) + ".properties"));
+              this.getClass().getResourceAsStream((("/testdata/" + envName) + ".properties"));
             propParams.load(inParam);
             inParam.close();
         } catch (IOException e) {
@@ -50,27 +44,18 @@ public abstract class AbstractAdVancedTestNGSpringContextTest extends
         }
     }
 
-    @DataProvider(
-            name = "loadParam"
-    )
+    @DataProvider(name = "loadParam")
     public Object[][] loadParam(Method method) {
         return TestUtils.loadParam(this.propParams, this.getClass().getSimpleName(), method);
     }
 
-    protected JsonNode generateParamJson(String reqParamsJsonStrAndAssert) {
-        String[] reqParamsJsonStrAndAssertArray = reqParamsJsonStrAndAssert.split(",assert=");
-        String reqParamsJsonStr = reqParamsJsonStrAndAssertArray[0];
-        log.debug(reqParamsJsonStr);
-        return JacksonUtils.toJsonNode(reqParamsJsonStr);
-    }
-
-
-
     protected JsonNode[] parseStr2JsonArray(String reqParamsJsonStrAndAssert) {
         JsonNode[] jsonNodes = new JsonNode[2];
-
-        String[] reqParamsJsonStrAndAssertArray = reqParamsJsonStrAndAssert.split(ASSERTION_SPLITSTRING);
-        jsonNodes[0] = JacksonUtils.toJsonNode(reqParamsJsonStrAndAssertArray[0]);
+        String[] reqParamsJsonStrAndAssertArray =
+          reqParamsJsonStrAndAssert.split(ASSERTION_SPLITSTRING);
+        String reqParamsJsonStr = reqParamsJsonStrAndAssertArray[0];
+        log.debug("param={}", reqParamsJsonStr);
+        jsonNodes[0] = JacksonUtils.toJsonNode(reqParamsJsonStr);
         if (reqParamsJsonStrAndAssertArray.length == PARAM_ASSERTION_ARRAY_SIZE) {
             jsonNodes[1] = JacksonUtils.toJsonNode(reqParamsJsonStrAndAssertArray[1]);
         }
