00e8c45b3661d0eea8814bd544a682ff54dc98bc
fastjrun <fastjrun@139.com>
2019-05-04 07:11:35 +0800

    调整了测试用例写法

--- codeg-test/src/test/java/com/fastjrun/codeg/service/CodeGServiceTest.java
+++ codeg-test/src/test/java/com/fastjrun/codeg/service/CodeGServiceTest.java
@@ -3,24 +3,13 @@
  */
 package com.fastjrun.codeg.service;
 
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Test;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fastjrun.codeg.common.CodeGConstants;
 import com.fastjrun.codeg.service.impl.DefaultCodeGService;
 import com.fastjrun.codeg.test.AbstractTestNGTest;
+import org.testng.annotations.Test;
 
 public class CodeGServiceTest extends AbstractTestNGTest implements CodeGConstants {
-
-    @BeforeClass
-    @org.testng.annotations.Parameters({
-            "envName"
-    })
-    public void setup(@Optional("local") String envName) {
-        this.initParam(envName);
-    }
 
     @Test(dataProvider = "loadParam")
     public void testGenerateAPI(String reqParamsJsonStrAndAssert) {
@@ -44,10 +33,11 @@ public class CodeGServiceTest extends AbstractTestNGTest implements CodeGConstan
         String bundleFiles = jsonNodes[0].get("bundleFiles").asText();
         String moduleName = jsonNodes[0].get("moduleName").asText();
         String packagePrefix = jsonNodes[0].get("packagePrefix").asText();
+        boolean supportServiceTest = jsonNodes[0].get("supportServiceTest").asBoolean();
         DefaultCodeGService codeGService = new DefaultCodeGService();
         codeGService.setPackageNamePrefix(packagePrefix);
         try {
-            codeGService.generateBundle(bundleFiles, moduleName, MockModel.MockModel_Common);
+            codeGService.generateProvider(bundleFiles, moduleName, supportServiceTest);
         } catch (Exception e) {
             this.processExceptionInResponse(jsonNodes[1], e);
         }
@@ -63,7 +53,7 @@ public class CodeGServiceTest extends AbstractTestNGTest implements CodeGConstan
         DefaultCodeGService codeGService = new DefaultCodeGService();
         codeGService.setPackageNamePrefix(packagePrefix);
         try {
-            codeGService.generateBundle(bundleFiles, moduleName, MockModel.MockModel_Swagger);
+            codeGService.generateProviderMock(bundleFiles, moduleName, MockModel.MockModel_Swagger);
         } catch (Exception e) {
             this.processExceptionInResponse(jsonNodes[1], e);
         }
@@ -96,7 +86,7 @@ public class CodeGServiceTest extends AbstractTestNGTest implements CodeGConstan
         DefaultCodeGService codeGService = new DefaultCodeGService();
         codeGService.setPackageNamePrefix(packagePrefix);
         try {
-            codeGService.generateBase(sqlFile, supportController, supportTest, moduleName);
+            codeGService.generateBase(sqlFile, moduleName, supportController, supportTest);
         } catch (Exception e) {
             this.processExceptionInResponse(jsonNodes[1], e);
         }
