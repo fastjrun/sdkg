fd1a5564185f99b6f984d37938cdf0a9f2757e56
fastjrun <fastjrun@139.com>
2019-05-04 07:00:07 +0800

    重构测试基类

--- sdkg-test/src/main/java/com/fastjrun/test/AbstractAdVancedTestNGSpringContextTest.java
+++ sdkg-test/src/main/java/com/fastjrun/test/AbstractAdVancedTestNGSpringContextTest.java
@@ -16,7 +16,6 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.util.Properties;
 
@@ -24,8 +23,6 @@ import java.util.Properties;
 public abstract class AbstractAdVancedTestNGSpringContextTest
   extends AbstractTestNGSpringContextTests {
 
-    protected static final String ASSERTION_SPLITSTRING      = ",assert=";
-    protected static final int    PARAM_ASSERTION_ARRAY_SIZE = 2;
 
     protected final Logger log = LoggerFactory.getLogger(this.getClass());
 
@@ -34,11 +31,9 @@ public abstract class AbstractAdVancedTestNGSpringContextTest
     @BeforeClass
     @Parameters({ "envName" })
     protected void initParam(@Optional("unitTest") String envName) {
+        log.debug("initParam");
         try {
-            InputStream inParam =
-              this.getClass().getResourceAsStream((("/testdata/" + envName) + ".properties"));
-            propParams.load(inParam);
-            inParam.close();
+            this.propParams = TestUtils.initParam("/testdata/" + envName + ".properties");
         } catch (IOException e) {
             log.error("load config for error:{}", e);
         }
@@ -46,19 +41,12 @@ public abstract class AbstractAdVancedTestNGSpringContextTest
 
     @DataProvider(name = "loadParam")
     public Object[][] loadParam(Method method) {
+        log.debug("loadParam");
         return TestUtils.loadParam(this.propParams, this.getClass().getSimpleName(), method);
     }
 
     protected JsonNode[] parseStr2JsonArray(String reqParamsJsonStrAndAssert) {
-        JsonNode[] jsonNodes = new JsonNode[2];
-        String[] reqParamsJsonStrAndAssertArray =
-          reqParamsJsonStrAndAssert.split(ASSERTION_SPLITSTRING);
-        String reqParamsJsonStr = reqParamsJsonStrAndAssertArray[0];
-        log.debug("param={}", reqParamsJsonStr);
-        jsonNodes[0] = JacksonUtils.toJsonNode(reqParamsJsonStr);
-        if (reqParamsJsonStrAndAssertArray.length == PARAM_ASSERTION_ARRAY_SIZE) {
-            jsonNodes[1] = JacksonUtils.toJsonNode(reqParamsJsonStrAndAssertArray[1]);
-        }
-        return jsonNodes;
+        log.debug("reqParamsJsonStrAndAssert={}", reqParamsJsonStrAndAssert);
+        return TestUtils.parseStr2JsonArray(reqParamsJsonStrAndAssert);
     }
 }
