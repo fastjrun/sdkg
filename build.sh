e7f2e9b81f6bf997ffeaf3505c3f9c03ad209c65
fastjrun <fastjrun@139.com>
2021-06-14 18:35:46 +0800

    升级3.0

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 echo "build ..."
 if [ "local_helper" = $1 ] ; then
-    mvn clean install -pl sdkg-helper -am
+    mvn clean install -pl
 elif [ "publish_helper" = $1 ] ; then
-    mvn -U clean deploy -pl sdkg-helper -am
+    mvn -U clean deploy -Prelease
 fi
 echo "build done."
